b9b7a00fd976273320f266408f077addbe2f2a24
Stuart Urback <stuart@urback.net>
2016-01-28 10:52:44 -0700

    Set up environment keys

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2] }))
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', routes);
 app.use('/users', users);
